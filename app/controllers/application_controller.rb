f8d77ba48920a40a0d6efd850e49d5a7caccedbf
Seunggyu <seunggyul14@icloud.com>
2023-01-30 18:35:35 -0800

    commit tree fb2752b37b35aa0c74a580d98ec5de0bb202c7bd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    def all
+        render json: Student.all, status: :ok
+    end
+
+    def grades
+        render json: Student.order('grade DESC')
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/students', to: 'application#all'
+  get '/students/grades', to: 'application#grades'
 end
